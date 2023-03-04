070c83c90a5904e277c066e59051694166d5236a
champagnekapy <kapuntimi@gmail.com>
2023-03-04 20:20:20 +0100

    revert

--- src/App.js
+++ src/App.js
@@ -2,35 +2,14 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-<<<<<<< HEAD
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Reacta
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
+        <p>Prvi commit.</p>
       </header>
     </div>
   );
-=======
-	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>Prvi commit.</p>
-			</header>
-		</div>
-	);
->>>>>>> c8fb4fe9bf2d160ff6b9034e3504090341f61321
 }
 
 export default App;
