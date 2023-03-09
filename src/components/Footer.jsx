c3fd328a0ac45637425fb48253a6d48202a94d02
Vuk Milenkovic <vukmilenkovic9@gmail.com>
2023-03-09 12:57:38 +0200

    Further changed the design of the Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,8 +9,27 @@ function Footer() {
       animate={{ opacity: 1 }}
       exit={{ opacity: 0, transition: { duration: 1 } }}
     >
-      <h2>Footer</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum aut quisquam provident! Quis placeat cupiditate quam sunt. Veritatis autem adipisci sint repudiandae esse quia quasi qui aut? Nisi, hic ducimus.</p>
+      
+      <div className="address">
+      <h3>Address</h3>
+      <p>Some Street 69</p>
+      <p>28300 Greensberg</p>
+
+      </div>
+
+      <div className="links">
+      
+      <ul><h3>You can find us here:</h3>
+      <li><i class="fa-brands fa-instagram"></i>  <a href="">Instagram</a></li>
+      <li><i class="fa-brands fa-facebook"></i>  <a href="">Facebook</a></li>
+      <li><i class="fa-brands fa-twitter"></i>  <a href="">Twitter</a></li>
+      <li><i class="fa-solid fa-envelope"></i>  <a href="">Email</a></li>
+    </ul>
+      </div>
+
+      <div className="contact">
+        <h3>Contact</h3>
+      </div>
     </motion.div>
     
   );
