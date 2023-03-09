8773ef195a89790c419a2c85882ed19b4eedc5b7
rika <rika.horvat@gmail.com>
2023-03-09 13:34:54 +0100

    vprasanje

--- src/App.js
+++ src/App.js
@@ -2,19 +2,19 @@ import Navbar from "./components/Navbar";
 import AnimatedRoutes from "./components/AnimatedRoutes";
 import Footer from "./components/Footer";
 import { BrowserRouter as Router } from "react-router-dom";
-
+// a tukaj manjka se import Card from "./components/Card"_I am so lost;
 function App() {
-  return (
-    <div className='App'>
-      <Router>
-        <Navbar />
-        <div className='content'>
-          <AnimatedRoutes />
-        </div>
-        <Footer />
-      </Router>
-    </div>
-  );
+	return (
+		<div className="App">
+			<Router>
+				<Navbar />
+				<div className="content">
+					<AnimatedRoutes />
+				</div>
+				<Footer />
+			</Router>
+		</div>
+	);
 }
 
 export default App;
